4bdd619de6226062affba8552ce819709558bcd6
John Saterfiel <j.saterfiel@gmail.com>
2017-09-14 20:38:41 -0400

    fixed class setup

--- gedcom/families.py
+++ gedcom/families.py
@@ -1,5 +1,19 @@
+"""Families GEDCOM
+Parses
+"""
+
 class Families:
+    """Families class
+    """
     def __init__(self):
         self._families = []
 
-    def processLineData(self, data):
+    def process_line_data(self, data):
+        """process_line_data will process line data
+        """
+        self._families.append(data)
+
+    def hello_world(self):
+        """hello
+        """
+        self._families = []
