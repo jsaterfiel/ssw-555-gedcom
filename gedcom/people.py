64a86cdc5c6bb43f150201d61bb4ef9dcc741145
chindsch <31941342+chindsch@users.noreply.github.com>
2017-11-01 20:13:56 -0400

    updating

--- gedcom/people.py
+++ gedcom/people.py
@@ -148,6 +148,43 @@ class People(object):
         print("Single Individuals")
         print(table)
 
+    def us35_print_recent_births(self):
+        """" US35 Print births in the last 30 days in pretty table
+        """
+        people = self.individuals
+        table = PrettyTable(["ID", "Name", "Birthdate"])
+
+        for person_id in people:
+            person = self.individuals[person_id]
+            recent_date = self._current_time - timedelta(days=30)
+            if person.get_birth_date() is None:
+                pass
+            if person.get_birth_date() is not None:
+                if recent_date < person.get_birth_date() and person.get_birth_date() < self._current_time:
+                    table.add_row([person.get_person_id(), person.get_name(), person.get_birth_date()])
+
+        print("Recent Births")
+        print(table)
+
+    def us36_print_recent_deaths(self):
+        """" US35 Print deaths in the last 30 days in pretty table
+        """
+        people = self.individuals
+        table = PrettyTable(["ID", "Name", "Deathdate"])
+
+        for person_id in people:
+            person = self.individuals[person_id]
+            recent_date = self._current_time - timedelta(days=30)
+
+            if person.get_death_date() is None:
+                pass
+            if person.get_death_date() is not None:
+                if recent_date < person.get_death_date() and person.get_death_date() < self._current_time:
+                    table.add_row([person.get_person_id(), person.get_name(), person.get_death_date()])
+
+        print("Recent Deaths")
+        print(table)
+
     def _us03_is_valid_birth_date(self, person):
         """US03 checks if birthday occurs after death
         Args:
